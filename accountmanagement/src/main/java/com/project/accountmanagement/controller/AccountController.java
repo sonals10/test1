3b4a7630d5b94d11a35df14eaf11d17dae15ef56
stealth1998 <sachinjain.ak47@gmail.com>
2022-07-26 17:25:01 +0530

    Created routes for deposit, withdrawal and transfer

--- accountmanagement/src/main/java/com/project/accountmanagement/controller/AccountController.java
+++ accountmanagement/src/main/java/com/project/accountmanagement/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.project.accountmanagement.controller;
 import java.sql.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,45 +13,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.accountmanagement.entity.AccountTransaction;
 import com.project.accountmanagement.entity.BankAccount;
+import com.project.accountmanagement.service.AccountService;
+import com.project.accountmanagement.service.ManagerService;
 
 @RestController
 public class AccountController {
+	@Autowired
+	private AccountService accountservice;
 	
-	@GetMapping("/accounts/{id}")
-	public List<BankAccount> getAccountsByCustomerId(@PathVariable long id){
-		
-		return 
-	}
-	
-	@GetMapping("/statement/{id}")
-	public List<AccountTransaction> getStatementsByAccountId(@PathVariable long accountId){
-//		Sysout
-		return 
-	}
-	@GetMapping("/statement/{date}")
-	public List<AccountTransaction> getStatementsByDate(@PathVariable Date date){
-		
-		return 
-	}
+//	@GetMapping("/accounts/{id}")
+//	public List<BankAccount> getAccountsByCustomerId(@PathVariable long id){
+//		
+//		return 
+//	}
+//	
+//	@GetMapping("/statement/{id}")
+//	public List<AccountTransaction> getStatementsByAccountId(@PathVariable long accountId){
+////		Sysout
+//		return 
+//	}
+//	@GetMapping("/statement/{date}")
+//	public List<AccountTransaction> getStatementsByDate(@PathVariable Date date){
+//		
+//		return 
+//	}
 	
 	@PostMapping("/deposit")
-	public AccountTransaction newTransactionByDeposit(@RequestParam(value="amount") int amount,
-			@RequestParam(value="accountnumber") int accountnumber){
+	public AccountTransaction newTransactionByDeposit(@RequestParam(value="amount") long amount,
+			@RequestParam(value="accountnumber") long accountnumber){
 		
-		return 
+		return accountservice.depositamount(amount,accountnumber);
 	}
 	
 	@PostMapping("/withdrawal")
-	public AccountTransaction newTransactionByWithdrawal(@RequestParam(value="amount") int amount,
-			@RequestParam(value="accountnumber") int accountnumber){
+	public AccountTransaction newTransactionByWithdrawal(@RequestParam(value="amount") long amount,
+			@RequestParam(value="accountnumber") long accountnumber){
 		
-		return 
+		return accountservice.withdrawamount(amount,accountnumber); 
 	}
 	
 	@PostMapping("/transfer")
 	public AccountTransaction newTransactionByTransfer(@RequestParam(value="amount") int amount,
 			@RequestParam(value="accountnumber1") int accountnumber1, @RequestParam(value="accountnumber2") int accountnumber2){
 		
-		return 
+		return accountservice.transferamount(amount,accountnumber1,accountnumber2);
 	}
 }
--- accountmanagement/src/main/java/com/project/accountmanagement/repository/BankAccountRepository.java
+++ accountmanagement/src/main/java/com/project/accountmanagement/repository/BankAccountRepository.java
@@ -1,9 +1,32 @@
 package com.project.accountmanagement.repository;
 
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.accountmanagement.entity.BankAccount;
+import com.project.accountmanagement.entity.Customer;
 
-public interface BankAccountRepository extends JpaRepository<BankAccount,Long>{
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount,Long>{	
+	
+	
+	
+	
+	@Modifying
+    @Query(value = "update bank_account set current_balance=:currentbalance where acc_number=:accountnumber ", nativeQuery = true)
+    @Transactional
+    void setAccountBalance(@Param("currentbalance") long currentbalance,@Param("accountnumber") long accountnumber);
+
+	BankAccount getByAccountNumber(long accountnumber);
+	
+	
+	
+//	@Query("")
+//	public void setAccountBalance(int currentbalance);
+	
 
 }
